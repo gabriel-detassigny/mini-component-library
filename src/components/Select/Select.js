e3867a2692461fb9321967b33597aefd8d33fcb0
Gabriel de Tassigny <gabriel@multivac.cc>
2022-06-15 10:52:30 +0900

    Add select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,49 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <Selector value={value} onChange={onChange}>
+        {children}
+      </Selector>
+      <SelectIcon id={'chevron-down'} size={24} strokeWidth={2}></SelectIcon>
+    </Wrapper>
   );
 };
+
+const SelectIcon = styled(Icon)`
+  display: inline-block;
+  position: absolute;
+  right: 12px;
+  top: 10px;
+`;
+
+const Wrapper = styled.div`
+  padding: 12px 52px 12px 16px;
+  background-color: ${COLORS.transparentGray15};
+  width: fit-content;
+  border-radius: 8px;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:focus-within {
+    outline: medium auto currentColor;
+    outline: medium auto invert;
+    outline: 5px auto -webkit-focus-ring-color;  
+  }
+  
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Selector = styled.select`
+  appearance: none;
+  background: none;
+  border: none;
+  color: inherit;
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default Select;
